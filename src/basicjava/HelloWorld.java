5d4f64481fb507ac2d6e7446e0b4eddefeb9a8e2
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-01-05 07:04:09 +0530

    commit

--- src/basicjava/HelloWorld.java
+++ src/basicjava/HelloWorld.java
@@ -3,5 +3,7 @@ package basicjava;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.printf("%d",24);
+//        System.out.printf(24);
     }
 }
