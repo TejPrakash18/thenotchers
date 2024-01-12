509fed46e8c20ea3724add7de90f1c24b578c165
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-01-12 17:50:08 +0530

    Commit

--- src/Leetcode/TwoPointerApporach/TwoSum.java
+++ src/Leetcode/TwoPointerApporach/TwoSum.java
@@ -1,4 +1,25 @@
 package Leetcode.TwoPointerApporach;
 
+class SolutionTwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
 public class TwoSum {
+    public static void main(String[] args) {
+        SolutionTwoSum obj = new SolutionTwoSum();
+        int arr[] = {2,7,11,15};
+        int target = 9;
+        int[] res =  obj.twoSum(arr,target);
+        for(int i=0; i<res.length; i++){
+            System.out.println(res[i]);
+        }
+    }
 }
