e3609ba22e0ac4ed1bc6f79ca6a7f72ba8869b14
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-01-20 19:34:57 +0530

    Array Reverse

---
+++ src/DataStructure/Arrays/ReverseArray.java
@@ -0,0 +1,24 @@
+package DataStructure.Arrays;
+
+public class ReverseArray {
+    public static void print(int arr[]){
+        int s = 0;
+        int e = arr.length-1;
+
+        while(s<e){
+            int temp = arr[s];
+            arr[s] = arr[e];
+            arr[e] = temp;
+            s++;
+            e--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,2,3,1};
+        print(arr);
+        for (int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+}
