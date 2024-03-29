cd6d5c6a8b356f32987901475e7cdc250f94def7
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-03-29 06:23:42 +0530

    BinaryAddition

--- src/BasicJava/LoopsProblems/AddTwoBinary.java
+++ src/BasicJava/LoopsProblems/AddTwoBinary.java
@@ -1,11 +1,29 @@
 package BasicJava.LoopsProblems;
 
 public class AddTwoBinary {
+    public static String addBinary(String binary1, String binary2) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = binary1.length() - 1;
+        int j = binary2.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int digit1 = i >= 0 ? binary1.charAt(i--) - '0' : 0;
+            int digit2 = j >= 0 ? binary2.charAt(j--) - '0' : 0;
+
+            int sum = digit1 + digit2 + carry;
+            carry = sum / 2;
+            sb.insert(0, sum % 2);
+        }
+
+        return sb.toString();
+    }
     public static void main(String[] args) {
-        String str1 = "1010";
-        String str2 = "0101";
+        String binary1 = "1010";
+        String binary2 = "0011";
 
-
+        String sum = addBinary(binary1, binary2);
+        System.out.println("Sum "+ sum );
 
     }
 }
