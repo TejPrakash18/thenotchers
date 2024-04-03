7d3fc1dd1d97f02f53ad6a5d08b164b58bac5608
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-04-03 20:32:23 +0530

    leapYear

--- src/BasicJava/If_else/LeapYear.java
+++ src/BasicJava/If_else/LeapYear.java
@@ -1,7 +1,16 @@
 package BasicJava.If_else;
 
+import java.util.Scanner;
+
 public class LeapYear{
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+            System.out.println("Year is leap");
+        }
+        else {
+            System.out.println("year is non-leap");
+        }
     }
 }
