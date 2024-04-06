23cdd08017e3821e47fa44bcb372919ea89a2f83
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-04-06 20:38:46 +0530

    string

---
+++ src/BasicJava/StringProblems/BasicString.java
@@ -0,0 +1,10 @@
+package BasicJava.StringProblems;
+
+public class BasicString {
+    public static void main(String[] args) {
+        String name = "Tej";
+        StringBuilder name1 = new StringBuilder("Tej");
+        System.out.println(name1);
+        System.out.println(name);
+    }
+}
