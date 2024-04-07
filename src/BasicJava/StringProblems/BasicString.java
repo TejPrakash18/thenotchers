e66ee98b74c6d1ca6a0712875538fb715b673332
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-04-07 21:36:18 +0530

    string

--- src/BasicJava/StringProblems/BasicString.java
+++ src/BasicJava/StringProblems/BasicString.java
@@ -3,8 +3,13 @@ package BasicJava.StringProblems;
 public class BasicString {
     public static void main(String[] args) {
         String name = "Tej";
-        StringBuilder name1 = new StringBuilder("Tej");
+        StringBuilder name1 = new StringBuilder("Tej18");
+        StringBuffer name2 = new StringBuffer("Neha");
         System.out.println(name1);
         System.out.println(name);
+        System.out.println("TejPrakash18");
+        System.out.println(name2);
+
+
     }
 }
