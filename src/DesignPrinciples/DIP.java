4f4bfa7d304ae1accf9d8657f07d63804122cb89
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-04-23 16:38:35 +0530

    DIP

---
+++ src/DesignPrinciples/DIP.java
@@ -0,0 +1,10 @@
+package DesignPrinciples;
+
+
+
+//Dependency Inversion Principal
+public class DIP {
+    public static void main(String[] args) {
+        System.out.println("Dependency Inversion Principal");
+    }
+}
