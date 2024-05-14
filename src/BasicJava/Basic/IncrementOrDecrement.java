831dbb4d9173ab1cff8323050c613eea25cfd144
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-05-14 21:38:44 +0530

    IncrementOrDecrement

--- src/BasicJava/Basic/IncrementOrDecrement.java
+++ src/BasicJava/Basic/IncrementOrDecrement.java
@@ -1,7 +1,21 @@
 package BasicJava.Basic;
 
+class Number{
+    int x;
+    void store(Number num){
+        num.x++;
+    }
+}
 public class IncrementOrDecrement {
+
     public static void main(String[] args) {
+
+        Number num = new Number();
+        num.x=10;
+        num.store(num);
+        System.out.println(num.x);
+
+
         int x, m,n;
         m = 10;
         n = 15;
@@ -15,6 +29,21 @@ public class IncrementOrDecrement {
         System.out.println(y++); //9
         System.out.println(y); //10
         System.out.println(++y); //11
+
+        int mm = 0;
+        loop1: for (int i = 0; i < 10; i++) {
+            System.out.println(i + "i : " + mm);
+            loop2: for (int j = 0; j < 10; j++) {
+                System.out.println(j + "j : " + mm);
+                loop3: for (int k = 0; k < 10; k++) {
+                    System.out.println(k + "k : " + mm);
+                    System.out.println(++mm);
+                    if (k % 10 == 0) {
+                        continue loop2;
+                    }
+                }
+            }
+        }
 
 
     }
