19170146386102c31645d0c6086356ef7726264f
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-05-16 07:42:11 +0530

    OOps

---
+++ src/Oops/Inheritence/Excercise3/Question1.java
@@ -0,0 +1,28 @@
+package Oops.Inheritence.Excercise3;
+
+class Base {
+    int x=10;
+    void display(){
+        System.out.println("Base class");
+    }
+}
+class Derived extends Base{
+    int x = 20;
+    void display(){
+        System.out.println("Derived class");
+    }
+}
+public class Question1 {
+    public static void main(String[] args) {
+
+        Base b = new Base();
+        Derived d = new Derived();
+        Base bd = new Derived();
+        System.out.println(b.x);
+        System.out.println(d.x);
+        System.out.println(bd.x);
+        b.display();
+        d.display();
+        bd.display();
+    }
+}
