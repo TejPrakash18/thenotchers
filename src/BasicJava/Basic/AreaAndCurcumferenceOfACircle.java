adceb27038a0e3ea916bcf1dc1facd9df8d0ca6e
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-06-03 13:32:51 +0530

    AreaOfCircle

---
+++ src/BasicJava/Basic/AreaAndCurcumferenceOfACircle.java
@@ -0,0 +1,15 @@
+package BasicJava.Basic;
+
+import java.util.Scanner;
+
+public class AreaAndCurcumferenceOfACircle {
+    public static void main(String[] args) {
+        double PI = 3.14;
+        Scanner scan  = new Scanner(System.in);
+        System.out.println("Enter the Area of circle: ");
+        int r = scan.nextInt();
+        double A = (PI * r * r);
+        System.out.println("Area of cirle "+A);
+
+    }
+}
