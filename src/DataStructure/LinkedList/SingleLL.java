61954213da0c751065ed5e10c820d34c611e2085
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-06-15 11:55:19 +0530

    init

--- src/DataStructure/LinkedList/SingleLL.java
+++ src/DataStructure/LinkedList/SingleLL.java
@@ -8,17 +8,29 @@ class LLNode{
         this.data = data;
         this.next = next;
     }
-
 }
 public class SingleLL {
     public static void main(String[] args) {
-        LLNode node1 =new LLNode(12, null);
+        LLNode node1 = new LLNode(12, null);
         LLNode node2 = new LLNode(13, null);
+        LLNode node3 = new LLNode(14, null);
+        LLNode node4 = new LLNode(15, null);
+        LLNode node5 = new LLNode(16, null);
 
-        LLNode  address = node1.next = node2;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        LLNode current = node1;
+
+        while (current != null){
+            System.out.println(current.data);
+            current = current.next;
+        }
 
 
-        System.out.println(address.hashCode());
+
 
     }
 }
