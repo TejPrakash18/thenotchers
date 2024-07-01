e96e69257325cdda1e7efefdc04376baee91a9a8
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-07-01 13:12:58 +0530

    init

--- src/DataStructure/BitManipulation/BasicOperation.java
+++ src/DataStructure/BitManipulation/BasicOperation.java
@@ -4,12 +4,12 @@ public class BasicOperation {
     public static void main(String[] args) {
         int x = 12;
         int y = 10;
-        System.out.println("Bitwise AND of (" + x + " , " + y + ") is: " + (x & y)); // Output: 8
+        int z = 5;
+        System.out.println("Bitwise AND of (" + x + " , " + y + ") is: " + (x & y));
         System.out.println("Bitwise OR of (" + x + " , " + y + ") is: " + (x | y));
         System.out.println("Bitwise XOR of (" + x + " , " + y + ") is: " + (x ^ y));
-        int z = 5;
         System.out.println("Bitwise NOT of " + z + " is: " + ~z);
-// Output: -6
+
 
     }
 }
