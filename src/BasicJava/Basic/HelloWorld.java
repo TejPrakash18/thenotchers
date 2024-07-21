0dcc8355a3e737ab1fbdb7f1aaad018b5b5d0cd3
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-07-21 22:03:29 +0530

    record

--- src/BasicJava/Basic/HelloWorld.java
+++ src/BasicJava/Basic/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello java!");
+        new Namste(); // call by the Namste record file;
     }
 }
---
+++ src/BasicJava/Basic/Namste.java
@@ -0,0 +1,9 @@
+package BasicJava.Basic;
+
+public record Namste() {
+    // call this function in HelloWorld class file
+    static{
+        System.out.println("Hello");
+        System.exit(0);
+    }
+}
