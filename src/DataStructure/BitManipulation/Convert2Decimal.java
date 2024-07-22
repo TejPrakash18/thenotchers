306834f7752835a285f38dbd38bf942bb93e4490
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-07-22 13:25:13 +0530

    BinaryConversion

--- src/DataStructure/BitManipulation/Convert2Decimal.java
+++ src/DataStructure/BitManipulation/Convert2Decimal.java
@@ -3,13 +3,13 @@ package DataStructure.BitManipulation;
 public class Convert2Decimal {
     public static int decimal(String x){
         int len = x.length();
-        int p2 =1;
+        int position =1;
         int res =0;
         for(int i=len-1; i>=0; i--){
             if (x.charAt(i)=='1'){
-                res = res+p2;
+                res = res+position;
             }
-            p2 = p2*2;
+            position = position*2;
         }
 
         return res;
