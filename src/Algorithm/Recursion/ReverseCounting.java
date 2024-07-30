ad0428ab0a15da97f5ccb3df8f1ad3e30f18e978
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-07-30 16:56:52 +0530

    DSA

--- src/Algorithm/Recursion/ReverseCounting.java
+++ src/Algorithm/Recursion/ReverseCounting.java
@@ -1,13 +1,22 @@
 package Algorithm.Recursion;
 
 public class ReverseCounting {
-    static void printCounting(int n){
+    static void printCountingReverse(int n){
         if(n==0) return;
         System.out.print(n+" ");
+        printCountingReverse(n-1);
+    }
+
+    static void printCounting(int n){
+        if(n==0) return;
         printCounting(n-1);
+        System.out.print(n+" ");
+
     }
     public static void main(String[] args) {
         int n=10;
         printCounting(n);
+        System.out.println();
+        printCountingReverse(n);
     }
 }
