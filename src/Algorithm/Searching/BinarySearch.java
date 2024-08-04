fdf2098567ff660c0f180e28549896e3f0d46055
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-08-04 15:34:14 +0530

    DSA

--- src/Algorithm/Searching/BinarySearch.java
+++ src/Algorithm/Searching/BinarySearch.java
@@ -1,11 +1,12 @@
 package Algorithm.Searching;
 
 public class BinarySearch {
-    static int findElement(int[] arr, int target){
+   public static int findElement(int[] arr, int target){
         int s = 0;
-        int e = arr.length;
+        int e = arr.length-1;
+
         while (s<=e){
-            int mid  = (s+e)/2;
+            int mid  = s+(e-s)/2;
             if(arr[mid] == target){
                 return mid;
             } else if (target > mid) {
@@ -14,15 +15,19 @@ public class BinarySearch {
             else {
                 e = mid-1;
             }
-
         }
         return -1;
     }
     public static void main(String[] args) {
 
         int[] arr = {1,2,4,5,7,11,15,19,23};
-        int target = 23;
+        int target = 19;
         int ans  = findElement(arr, target);
-        System.out.println("Element found at index "+ans);
+
+        if(ans == -1){
+            System.out.println("Element not found");
+        }else {
+            System.out.println("Element found at index "+ans);
+        }
     }
 }
