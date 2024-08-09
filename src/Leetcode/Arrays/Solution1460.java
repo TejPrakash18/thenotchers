850dbb161cecd5c094d997ae7cdc386eed8c9f62
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-08-09 16:21:58 +0530

    DSA

--- src/Leetcode/Arrays/Solution1460.java
+++ src/Leetcode/Arrays/Solution1460.java
@@ -1,21 +1,13 @@
 package Leetcode.Arrays;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Solution1460 {
     public static boolean Make2ArraysEqualbyReversing(int[] target, int[] arr){
-        int ans=0;
+        Arrays.sort(target);
         Arrays.sort(arr);
-        int len = target.length;
-        for(int i=0; i<target.length;i++){
-            if(target[i] == arr[i]){
-               ans++;
-            }
-        }
-        if(ans == len){
-            return true;
-        }
-        return false;
+        return Arrays.equals(target,arr);
     }
     public static void main(String[] args) {
         int[] target = {392,360};
