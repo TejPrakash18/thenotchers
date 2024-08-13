00aacb9e74cf99535323eab332fb36bba2f4eba4
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-08-13 14:49:11 +0530

    bit manipulation

---
+++ src/DataStructure/BitManipulation/PowerOfTwo.java
@@ -0,0 +1,18 @@
+package DataStructure.BitManipulation;
+
+public class PowerOfTwo {
+    public static boolean powerOf2(int n){
+        if ((n & n-1)==0){
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        int n = 15;
+        boolean ans = powerOf2(n);
+        System.out.println(ans);
+    }
+}
