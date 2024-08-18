6d510401bf8f1579502447940ef40fe1f01bf232
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-08-18 19:00:54 +0530

    backtracking

--- src/DataStructure/Recursion/BackTracking/Permutation.java
+++ src/DataStructure/Recursion/BackTracking/Permutation.java
@@ -3,12 +3,29 @@ package DataStructure.Recursion.BackTracking;
 public class Permutation {
     public static void main(String[] args) {
         String str = "abc";
-        printPermutation(str);
+        int i=0;
+        printPermutation(str.toCharArray(),i);
     }
 
-    private static void printPermutation(String str) {
-        if(){
-            System.out.println();
+    private static void printPermutation(char[] str, int i) {
+        if(i >= str.length){
+            System.out.println(new String (str)+" ");
+            return;
+
         }
+        for (int j = i; j < str.length; j++) {
+
+            swap(str, j, i);
+            printPermutation(str,i+1);
+            swap(str,i,j); //backtracking
+
+        }
+
+    }
+
+    private static void swap(char[] arr ,int i ,int j){
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
